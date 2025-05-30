bc4bbe3f7d130f32f5878d313416b9f49396bb87
Taona Madawo <synthesis.co.za>
2025-05-30 14:33:34 +0200

    deploy to test destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,15 @@ terraform {
     bucket  = "kubernetesstate"
     prefix  = "terraform/state"
   }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
 }
+
 
 provider "google" {
   project = var.project_id
