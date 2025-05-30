49bd7340335140248779311a9cb648bd86dd6057
Taona Madawo <synthesis.co.za>
2025-05-30 13:08:35 +0200

    comment some line to see eeerrors source ranges

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,85 +1,85 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
+# provider "google" {
+#   project = var.project_id
+#   region  = var.region
+#   zone    = var.zone
+# }
 
-resource "google_compute_instance" "cks-master" {
-  name         = "cks-master"
-  machine_type = "e2-medium"
-  zone         = var.zone
+# resource "google_compute_instance" "cks-master" {
+#   name         = "cks-master"
+#   machine_type = "e2-medium"
+#   zone         = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2004-focal-v20220419"
-      size  = 50
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "ubuntu-2004-focal-v20220419"
+#       size  = 50
+#     }
+#   }
 
-  network_interface {
-    network = "default"
-    access_config {}
-  }
+#   network_interface {
+#     network = "default"
+#     access_config {}
+#   }
 
-  metadata = {
-    # ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-    ssh-keys = "ubuntu:${tls_private_key.vm_ssh.public_key_openssh}"
-  }
+#   metadata = {
+#     # ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+#     ssh-keys = "ubuntu:${tls_private_key.vm_ssh.public_key_openssh}"
+#   }
 
-  tags = ["cks-node"]
-}
+#   tags = ["cks-node"]
+# }
 
-resource "google_compute_instance" "cks-worker" {
-  name         = "cks-worker"
-  machine_type = "e2-medium"
-  zone         = var.zone
+# resource "google_compute_instance" "cks-worker" {
+#   name         = "cks-worker"
+#   machine_type = "e2-medium"
+#   zone         = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2004-focal-v20220419"
-      size  = 50
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "ubuntu-2004-focal-v20220419"
+#       size  = 50
+#     }
+#   }
 
-  network_interface {
-    network = "default"
-    access_config {}
-  }
+#   network_interface {
+#     network = "default"
+#     access_config {}
+#   }
 
-  metadata = {
-    # ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-    ssh-keys = "ubuntu:${tls_private_key.vm_ssh.public_key_openssh}"
-  }
+#   metadata = {
+#     # ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+#     ssh-keys = "ubuntu:${tls_private_key.vm_ssh.public_key_openssh}"
+#   }
 
-  tags = ["cks-node"]
-}
+#   tags = ["cks-node"]
+# }
 
-resource "google_compute_firewall" "nodeports" {
-  name    = "nodeports"
-  network = "default"
+# resource "google_compute_firewall" "nodeports" {
+#   name    = "nodeports"
+#   network = "default"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["30000-40000"]
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["30000-40000"]
+#   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["cks-node"]
-}
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags = ["cks-node"]
+# }
 
-resource "tls_private_key" "vm_ssh" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+# resource "tls_private_key" "vm_ssh" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
 
-# outputs.tf
-output "public_key_openssh" {
-  value = tls_private_key.vm_ssh.public_key_openssh
-}
+# # outputs.tf
+# output "public_key_openssh" {
+#   value = tls_private_key.vm_ssh.public_key_openssh
+# }
 
-output "private_key_pem" {
-  value     = tls_private_key.vm_ssh.private_key_pem
-  sensitive = true
-}
+# output "private_key_pem" {
+#   value     = tls_private_key.vm_ssh.private_key_pem
+#   sensitive = true
+# }
 
 
