db8bdd392945a7f0fb34fc7758e556473d6645a2
Taona Madawo <synthesis.co.za>
2025-06-01 20:41:35 +0200

    testing ssh from actions

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -1,35 +1,3 @@
-# #!/bin/bash
-
-# MASTER_IP=$(terraform -chdir=terraform output -raw master_ip)
-# WORKER_IP=$(terraform -chdir=terraform output -raw worker_ip)
-
-# cat <<EOF > ansible/inventory.ini
-# [master]
-# $MASTER_IP ansible_user=ubuntu
-
-# [worker]
-# $WORKER_IP ansible_user=ubuntu
-# EOF
-
-# echo "Inventory generated at ansible/inventory.ini"
-#!/bin/bash
-# cd terraform
-# terraform output -json > tfout.json
-# cd ..
-
-# MASTER_IP=$(jq -r '.instance_ips.value[0]' terraform/tfout.json)
-# WORKER_IP1=$(jq -r '.instance_ips.value[1]' terraform/tfout.json)
-# WORKER_IP2=$(jq -r '.instance_ips.value[2]' terraform/tfout.json)
-
-# cat <<EOF > ansible/inventory.ini
-# [masters]
-# $MASTER_IP ansible_user=ubuntu ansible_host=$MASTER_IP
-
-# [workers]
-# $WORKER_IP1 ansible_user=ubuntu ansible_host=$WORKER_IP1
-# $WORKER_IP2 ansible_user=ubuntu ansible_host=$WORKER_IP2
-# EOF
-
 #!/bin/bash
 set -e
 
@@ -49,7 +17,7 @@ fi
 
 MASTER_IP=$(echo "$IP_LIST" | sed -n 1p)
 WORKER_IP1=$(echo "$IP_LIST" | sed -n 2p)
-# WORKER_IP2=$(echo "$IP_LIST" | sed -n 3p)
+
 
 cat <<EOF > ansible/inventory.ini
 [masters]
@@ -57,7 +25,7 @@ $MASTER_IP ansible_user=ubuntu ansible_host=$MASTER_IP
 
 [workers]
 $WORKER_IP1 ansible_user=ubuntu ansible_host=$WORKER_IP1
-# $WORKER_IP2 ansible_user=ubuntu ansible_host=$WORKER_IP2
+
 EOF
 
 echo "✅ Inventory generated at ansible/inventory.ini"
