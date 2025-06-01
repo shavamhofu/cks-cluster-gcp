d4042bda8d4642dead807045c83075c9c759bd8b
Taona Madawo <synthesis.co.za>
2025-06-01 23:15:28 +0200

    testing ssh from actions

--- scripts/generate_inventory_new.sh
+++ scripts/generate_inventory_new.sh
@@ -12,10 +12,10 @@ fi
 
 cat > ansible/inventory.ini <<EOF
 [masters]
-$MASTER_IP ansible_user=ubuntu ansible_host=$MASTER_IP
+$MASTER_IP ansible_user=ubuntu ansible_host=$MASTER_IP ansible_ssh_private_key_file=~/.ssh/id_rsa
 
 [workers]
-$WORKER_IP ansible_user=ubuntu ansible_host=$WORKER_IP
+$WORKER_IP ansible_user=ubuntu ansible_host=$WORKER_IP ansible_ssh_private_key_file=~/.ssh/id_rsa
 EOF
 
 echo "✅ Inventory generated at ansible/inventory.ini"
