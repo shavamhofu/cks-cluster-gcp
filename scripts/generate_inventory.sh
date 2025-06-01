e29b40c73f5f2dc2ac1e385eaf7870b7e85bd7b9
Taona Madawo <synthesis.co.za>
2025-06-01 13:48:50 +0200

    apply after generate ni

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -5,10 +5,10 @@ WORKER_IP=$(terraform -chdir=terraform output -raw worker_ip)
 
 cat <<EOF > ansible/inventory.ini
 [master]
-$MASTER_IP ansible_user=ubuntu
+$MASTER_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
 
 [worker]
-$WORKER_IP ansible_user=ubuntu
+$WORKER_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
 EOF
 
 echo "Inventory generated at ansible/inventory.ini"
