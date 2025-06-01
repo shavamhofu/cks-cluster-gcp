fbddf46d1ab4ee6076ee66b12154dc90fe857ecc
Taona Madawo <synthesis.co.za>
2025-06-01 19:54:48 +0200

    testing ssh from actions

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "master_ip" {
 output "worker_ip" {
   value = google_compute_instance.cks-worker.network_interface[0].access_config[0].nat_ip
 }
+
+output "instance_ips" {
+  value = [
+    google_compute_instance.cks-master.network_interface[0].access_config[0].nat_ip,
+    google_compute_instance.cks-worker[0].network_interface[0].access_config[0].nat_ip,
+    # google_compute_instance.worker[1].network_interface[0].access_config[0].nat_ip
+  ]
+}
